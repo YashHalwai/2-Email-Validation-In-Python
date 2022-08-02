555bb06a0f4b71908223d2dbc38dbcca29ba2706
Yash Sanjeev Halwai <halwaiyash45@gmail.com>
2022-08-02 15:41:43 +0530

    Regex

---
+++ main.py
@@ -0,0 +1,18 @@
+# Email Validation in Python (Using Regex)
+
+# a - z
+# 0 - 9
+# . _ time 1
+# @ time 1
+# . 2, 3
+
+import re
+
+email_condition = "^[a-z]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$"
+user_email = input("Enter your Email: ")
+
+if re.search(email_condition,user_email):
+    print("Right Email")
+else:
+    print("Invalid Email")
+
